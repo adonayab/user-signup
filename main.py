01ca67373018e7f0e8452016430f482fe46d999e
Adonay Gebre <adonayab15@hotmail.com>
2019-02-04 21:18:22 -0600

    Returns all error messages instead of only one

--- main.py
+++ main.py
@@ -28,6 +28,27 @@ def index():
       
       return render_template('/index.html', empty_form_error=empty_form_error, username_error=username_error, password_error=password_error, pass_confirm_error=pass_confirm_error)
     
+
+
+    pattern = re.compile(r'^[a-zA-Z0-9_-]{3,20}$')
+    valid_username = re.findall(pattern, username)
+    if username not in valid_username:
+        username_error = 'Invalid username'
+    if len(password) < 3 or len(password) > 20:
+        password_error = 'Invalid Password'
+    if password != pass_confirm:
+        pass_confirm_error = "Password don't match"
+        
+    pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+    valid_email = re.findall(pattern, email)
+    if email == '':
+      pass
+    elif email not in valid_email:
+        email_error = 'Invalid email address'
+        return render_template('/index.html', username_error=username_error, password_error=password_error, pass_confirm_error=pass_confirm_error, email_error=email_error)
+
+
+
     pattern = re.compile(r'^[a-zA-Z0-9_-]{3,20}$')
     valid_username = re.findall(pattern, username)
     if username not in valid_username:
@@ -37,8 +58,12 @@ def index():
     else:
       if len(password) < 3 or len(password) > 20:
         password_error = 'Invalid Password'
-        return render_template('/index.html', password_error=password_error)
+      if password != pass_confirm:
+        pass_confirm_error = "Password don't match"
+        return render_template('/index.html', password_error=password_error, pass_confirm_error=pass_confirm_error)
       
+
+
       if password != pass_confirm:
         pass_confirm_error = "Password don't match"
         return render_template('/index.html', pass_confirm_error=pass_confirm_error)
@@ -47,8 +72,8 @@ def index():
       pattern = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
       valid_email = re.findall(pattern, email)
       if email == '':
-        redirect('/welcome?username={}'.format(username), code=302)
-      # elif ('@' not in email) and ('.' not in email):
+        pass
+        # redirect('/welcome?username={}'.format(username), code=302)
       elif email not in valid_email:
         email_error = 'Invalid email address'
         return render_template('/index.html', email_error=email_error)
